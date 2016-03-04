78d6857412b3f894695dd8ff0e2ed8b4d6e375eb
benkettlewell <ben.kettlewell@gmail.com>
2016-03-04 15:14:51 -0800

    bunch of changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -12,6 +12,42 @@ class PinsController < ApplicationController
   def show
     @pin = Pin.find(params[:id])
   end
-  
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.create(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to "/pins/name-#{@pin.slug}"
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
+  def edit
+    logger.info params[:slug]
+    @pin = Pin.find_by_slug (params[:id][5..-1])
+  end
+
+  def update
+    @pin = Pin.find_by_id (params[:id])
+    @pin.update pin_params
+    if @pin.valid?  
+      @pin.save
+      redirect_to "/pins/name-#{@pin.slug}"
+    else
+      @errors = @pin.errors
+      render :edit
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
 end
